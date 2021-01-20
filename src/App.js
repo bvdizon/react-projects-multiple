5af4f5d5a7dc9770864ec5a3f933dfa5732e27a0
briandizon <bvdizon@gmail.com>
2021-01-21 05:17:15 +0800

    lorem ipsum generator, first commit, complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+// locally hosted data
+import data from './data';
 
 function App() {
+  // 'text' state will be set based on the 'count' state
+  // 'count' state is being set by a form onSubmit,
+  // it determines how many paragraphs will be set to 'text' state
+  const [text, setText] = useState([]);
+  const [count, setCount] = useState(1);
+
+  // using javascript '.splice()' function, local 'data' will be
+  // fetched and saved to 'text' state
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newData = data.map((data) => data);
+    setText(newData.splice(0, count));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='section'>
+      <h2>Lorem Ipsum Generator</h2>
+      <h4>Choose the number of paragraphs to generate.</h4>
+      <form onSubmit={handleSubmit}>
+        <button>Generate</button>
+        <input
+          type='number'
+          name='count'
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+          min='1'
+          max='8'
+        />
+      </form>
+
+      {/* 
+        'text' state is now set with data based on the logic in 'handleSubmit 
+        and will be conditionally displayed using short-circuit evaluation '&&'
+      */}
+      {text && text.map((text, index) => <p key={index}>{text}</p>)}
     </div>
   );
 }
